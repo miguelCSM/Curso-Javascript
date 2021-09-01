dbfdf34b579829cdab78c6f4682806b2a0ba3549
mikegomdi <mikegomez1493@gmail.com>
2021-08-31 19:29:30 -0500

    funciones

---
+++ js/11_funciones.js
@@ -0,0 +1,14 @@
+document.write("<h1>Funciones</h1>");
+
+// Cuando no reciben parámetros de entrada
+function saludar(){
+    return document.write("<p>Hola a todos</p>");
+}
+//saludar();
+
+// funcion con parametros de entrada
+function concatena_el_nombre(nombre, apellidoP, apellidoM) {
+    return nombre,apellidoP,apellidoM;
+}
+
+concatena_el_nombre();
