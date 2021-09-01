63ab2aec938e574c460e25d1c2505b827c7c1e5f
mikegomdi <mikegomez1493@gmail.com>
2021-08-31 20:12:59 -0500

    funciones-anidadas

--- js/11_funciones.js
+++ js/11_funciones.js
@@ -1,14 +1,39 @@
 document.write("<h1>Funciones</h1>");
 
 // Cuando no reciben parámetros de entrada
-function saludar(){
+function saludar() {
     return document.write("<p>Hola a todos</p>");
 }
 //saludar();
 
 // funcion con parametros de entrada
 function concatena_el_nombre(nombre, apellidoP, apellidoM) {
-    return nombre,apellidoP,apellidoM;
+    var nombreC = nombre + apellidoP + apellidoM;
+    document.write(nombreC);
+    return "nombreC";
+
 }
 
-concatena_el_nombre();
+/*var nombrecompletop = concatena_el_nombre("Miguel", "gómez", "díaz");
+console.log(nombrecompletop)*/
+
+// Funcion con parametros opcionales
+function calculadora(num1, num2, mostrar = true) {
+    //Solo imprimirá en consola los valores calculados
+    if (mostrar == true) {
+        console.log("Se presentan resultados en la consola");
+        console.log(num1 + num2);
+        console.log(num1 - num2);
+        console.log(num1 * num2);
+        console.log(num1 / num2);
+    } else {
+        console.log("Se presentan resultados en el documento");
+        document.write("<p>",num1 + num2,"</p>");
+        document.write("<p>",num1 - num2,"</p>");
+        document.write("<p>",num1 * num2,"</p>");
+        document.write("<p>",num1 / num2,"</p>");
+    }
+
+}
+
+calculadora(2, 4);
